650fffe764f8fa7fbf521811008fc2deef926f62
SIDHARTHA <sidhartha758013@gmail.com>
2021-07-14 18:27:21 +0530

    Testing Flutter web deploy manually/automatic method

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Icon(Icons.add,color: Colors.red,),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
